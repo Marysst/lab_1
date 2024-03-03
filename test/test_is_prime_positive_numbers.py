3d8908a16853bcea56bd69267cb9d22159ba6a48
Marysst <mariua22102004@gmail.com>
2024-03-03 19:25:21 +0200

    Added test is prime positive numbers

---
+++ test/test_is_prime_positive_numbers.py
@@ -0,0 +1,17 @@
+import unittest
+import sys
+
+sys.path.append("..")  
+from is_prime import is_prime
+
+class TestIsPrime(unittest.TestCase):
+    def test_is_prime_positive_numbers(self):
+        self.assertTrue(is_prime(2))
+        self.assertTrue(is_prime(3))
+        self.assertTrue(is_prime(5))
+        self.assertTrue(is_prime(7))
+        self.assertTrue(is_prime(11))
+        self.assertTrue(is_prime(23))
+
+if __name__ == '__main__':
+    unittest.main()
