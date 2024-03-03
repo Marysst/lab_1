a3bd31996870ecb2a8c6616892c5ee3ec515d667
Marysst <mariua22102004@gmail.com>
2024-03-03 19:15:00 +0200

    Added test is prime custom divisor

---
+++ test/test_is_prime_custom_divisor.py
@@ -0,0 +1,17 @@
+import unittest
+import sys
+
+sys.path.append("..")  
+from is_prime import is_prime
+
+class TestIsPrime(unittest.TestCase):
+    def test_is_prime_custom_divisor(self):
+        self.assertTrue(is_prime(13, 4))
+        self.assertFalse(is_prime(15, 4))
+        self.assertTrue(is_prime(17, 5))
+        self.assertFalse(is_prime(18, 5))
+        self.assertTrue(is_prime(23, 7))
+        self.assertFalse(is_prime(54, 6)
+
+if __name__ == '__main__':
+    unittest.main()
