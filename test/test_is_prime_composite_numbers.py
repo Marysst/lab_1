260317c605b1fb7be908a6644de5014d47f5866b
Marysst <mariua22102004@gmail.com>
2024-03-03 19:12:01 +0200

    Added test is prime composite numbers

---
+++ test/test_is_prime_composite_numbers.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+
+sys.path.append("..")  
+from is_prime import is_prime
+
+class TestIsPrime(unittest.TestCase):
+        def test_is_prime_composite_numbers(self):
+                self.assertFalse(is_prime(4))
+                self.assertFalse(is_prime(12))
+                self.assertFalse(is_prime(25))
+                self.assertFalse(is_prime(49))
+                self.assertFalse(is_prime(93))
+
+if __name__ == '__main__':
+    unittest.main()
