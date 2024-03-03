8a59ab5f72c918388995a0365bc97daaf98ab880
Marysst <mariua22102004@gmail.com>
2024-03-03 19:21:13 +0200

    Added test is prime negative numbers

---
+++ test/test_is_prime_negative_numbers.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+
+sys.path.append("..")  
+from is_prime import is_prime
+
+class TestIsPrime(unittest.TestCase):
+    def test_is_prime_negative_numbers(self):
+        self.assertIsNone(is_prime(-2))
+        self.assertIsNone(is_prime(-3))
+        self.assertIsNone(is_prime(-5))
+        self.assertIsNone(is_prime(-7))
+        self.assertIsNone(is_prime(-9))
+
+if __name__ == '__main__':
+    unittest.main()
