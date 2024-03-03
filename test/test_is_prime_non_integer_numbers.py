ae502264b7323ead0a1484d9485a3f6b2106c209
Marysst <mariua22102004@gmail.com>
2024-03-03 19:23:19 +0200

    Added test is prime non integer numbers

---
+++ test/test_is_prime_non_integer_numbers.py
@@ -0,0 +1,17 @@
+import unittest
+import sys
+
+sys.path.append("..")  
+from is_prime import is_prime
+
+class TestIsPrime(unittest.TestCase):
+    def test_is_prime_non_integer_numbers(self):
+        self.assertIsNone(is_prime(2.5))
+        self.assertIsNone(is_prime(43.14))
+        self.assertIsNone(is_prime(90.57))
+        self.assertIsNone(is_prime(-4.7))
+        self.assertIsNone(is_prime(-36.56))
+        self.assertIsNone(is_prime(-85.89))
+
+if __name__ == '__main__':
+    unittest.main()
