80fd8bdbe24751652a82a1459f4e8221e6844262
Marysst <161862691+Marysst@users.noreply.github.com>
2024-03-04 15:03:38 +0200

    Update 'conftest.py' file for managing test environment settings
    Update a path to the parent directory where the file 'is_prime.py' under test is located

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,6 @@
 import os
 import sys
 
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(file), os.pardir))
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 sys.path.insert(0, parent_dir)
