f31c24652fcbf50d999dd49ead9815df4931a200
Marysst <161862691+Marysst@users.noreply.github.com>
2024-03-04 14:38:11 +0200

    Update test is prime composite numbers

--- test/test_is_prime_composite_numbers.py
+++ test/test_is_prime_composite_numbers.py
@@ -1,4 +1,10 @@
 import unittest
+import os
+import sys
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(file), os.pardir))
+
+sys.path.insert(0, parent_dir)
  
 from is_prime import is_prime
 
